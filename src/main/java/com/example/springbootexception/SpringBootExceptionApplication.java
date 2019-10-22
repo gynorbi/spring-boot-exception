974a7f4ccd1c9440bc2bbec61f95f8fcb39cf9bb
Norbert Gyarfas <norbert.gyarfas@ing.com>
2019-10-22 16:35:39 +0300

    Finished project

--- src/main/java/com/example/springbootexception/SpringBootExceptionApplication.java
+++ src/main/java/com/example/springbootexception/SpringBootExceptionApplication.java
@@ -1,11 +1,18 @@
 package com.example.springbootexception;
 
+import com.example.springbootexception.exceptions.MyHandlerExceptionResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
 public class SpringBootExceptionApplication {
 
+	@Bean
+	HandlerExceptionResolver customExceptionResolver () {
+		return new MyHandlerExceptionResolver();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExceptionApplication.class, args);
 	}
